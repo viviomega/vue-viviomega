7644d4e64ba361a7eed6e29e6f717f0c6ca03bd6
Shunsuke.Nakajima <shunsuke.skillup@outlook.jp>
2023-06-12 09:19:32 +0900

    【Vuetify 3】Headerを設置する

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <v-app>
+    <v-app-bar color="primary">
+      <template v-slot:prepend>
+        <v-icon aria-hidden="false"> {{ home.icon }} </v-icon>
+      </template>
+      <template v-slot:title>
+        <v-app-bar-title>{{ title }}</v-app-bar-title>
+      </template>
+    </v-app-bar>
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
-    //
+    title: "びびおめがサイト",
+    home: {
+      icon: "mdi-home",
+      link: "", //今回は設定しない
+    },
   }),
-}
+};
 </script>
